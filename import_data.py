8853c0f51dea4d6f1e4407297d9f30ba556d2735
hkolano <hannah.kolano@students.olin.edu>
2017-04-25 15:10:59 -0400

    preliminary parsing, does not work yet

--- import_data.py
+++ import_data.py
@@ -1,11 +1,38 @@
-import pandas as pd
 from pyexcel_ods import get_data
 import pyexcel as pe
 import json
 
-# data = get_data("consolidated_data.ods")
+data = get_data("consolidated_data.ods", start_row=1, row_limit=100, start_column=2, column_limit=3)
+data = data["Sheet1"]
 
-sheet = pe.get_book(file_name="consolidated_data.ods")
-print(sheet)
-# for i in range(len(data)-1):
-#     print(json.dumps(data[i]))
+cases = []
+judges = []
+first_case = data[0:8]
+
+for row in first_case:
+    # initializes judge indexes
+    judges.append(row[1])
+
+counter = 0
+for row in data:
+    if row[1] not in judges:
+        case_w_new_judge = row[0]
+        index_of_that = counter
+        counter += 1
+
+new_sheet = [[0]*int(index_of_that/9) for i in range(9)]
+
+for row in data:
+    case, leaning, judge = row[0], row[1], row[2]
+    # change all the 2's to -1's
+    if leaning == 2:
+        leaning = -1
+    # if it's a new case, add to the case dict and give a number to the case
+    if case not in cases:
+        cases.append(case)
+    # now add to the matrix!
+    new_sheet[judges.index(judge)][cases.index(case)] = leaning
+
+    row[0], row[1], row[2] = case, leaning, judge
+
+print(new_sheet)
