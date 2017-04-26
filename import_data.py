ee95be454770e80df963c4dcf5ebc0194516426f
hkolano <hannah.kolano@students.olin.edu>
2017-04-26 11:54:44 -0400

    prints list of matrices, with the beginning of each row the judge number followed by the decisions case-by-case in that court

--- import_data.py
+++ import_data.py
@@ -9,8 +9,6 @@ hannah.kolano@students.olin.edu
 '''
 # import the needed libraries
 from pyexcel_ods import get_data
-import pyexcel as pe
-import json
 
 # get data from the spreadsheet
 data = get_data("consolidated_data.ods", start_row=1, start_column=2, column_limit=3)
@@ -21,80 +19,67 @@ cases = []
 judges = []
 first_case = data[0:9]
 court_matrix = []
-case_info = []
 case_indexes = []
 
 # initializes judge list for the first case
 for row in first_case:
     judges.append(row[2])
 
+
 # creates the first matrix, of zeroes initially
 new_sheet = [[0]*int(len(data)/9+1) for i in range(9)]
+for judge in judges:
+    new_sheet[judges.index(judge)][0] = judge
 
 # some important counters
-old_num_justices = 9
 counter = 0
 case_in_court_count = 0
-
-# for row in data:
-#     case = row[0]
-#     if case not in cases:
-#         cases.append(case)
-#         case_indexes.append(cases.index(case))
-#     counter += 1
-
-cases = []
 
 # parses spreadsheet row by row
 # each row is a judge, rotating in intervals of 8/9
 for row in data:
     case, leaning, judge = row[0], row[1], row[2]
 
-    # if the judge is new, find the index of the last case without him/her
+    # if the judge is new...
     if judge not in judges:
         n = 1
         prev_case = data[counter-n][0]
+        # check where the case with a different court started
         while prev_case == case:
             n += 1
             prev_case = data[counter-n][0]
         new_case_row_index = counter-n+1
-        n = 1
-        num_justices = 0
-        this_case = data[new_case_row_index][0]
-        while this_case == case:
-            num_justices += 1
-            this_case = data[new_case_row_index+n][0]
-            n += 1
-        # reset all the variables
-        for n in range(old_num_justices):
-            new_sheet[n] = new_sheet[n][:int(case_in_court_count/old_num_justices)]
+        # get the zeroes off the end of each justices decisions
+        for n in range(9):
+            new_sheet[n] = new_sheet[n][:int(case_in_court_count/9)]
+        # finish this sheet and make a new one
         court_matrix.append(new_sheet)
-        new_sheet = [[0]*int(len(data)/num_justices+1) for i in range(num_justices)]
+        new_sheet = [[0]*int(len(data)/9+1) for i in range(9)]
         cases = []
         # reset the judges
-        new_first_case = data[new_case_row_index:new_case_row_index+num_justices]
-        # print(new_first_case)
+        new_first_case = data[new_case_row_index:new_case_row_index+9]
         judges = []
         for row in new_first_case:
             judges.append(row[2])
+        for judge in judges:
+            new_sheet[judges.index(judge)][0] = judge
         case_in_court_count = 0
-        old_num_justices = num_justices
     # change all the 2's to -1's
     if leaning == '':
         leaning = 0
     if leaning == 2:
         leaning = -1
-    # if it's a new case, add to the case dict and give a number to the case
+    # if it's a new case, add to the case list
     if case not in cases:
         cases.append(case)
-    # now add to the matrix!
-    new_sheet[judges.index(judge)][cases.index(case)] = leaning
+    # insert the decision into the proper cell in the sheet
+    new_sheet[judges.index(judge)][cases.index(case)+1] = leaning
 
     counter += 1
     case_in_court_count += 1
     row[0], row[1], row[2] = case, leaning, judge
 
-for n in range(num_justices):
-    new_sheet[n] = new_sheet[n][:int(case_in_court_count/num_justices)]
+for n in range(9):
+    new_sheet[n] = new_sheet[n][:int(case_in_court_count/9)]
 court_matrix.append(new_sheet)
 print(court_matrix)
